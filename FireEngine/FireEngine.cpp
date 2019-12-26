052f2b6ab65d5e21083d4451626252fd329ba4f9
Ruilan Berg Pereira <rui_berg@hotmail.com>
2019-12-26 16:51:24 -0300

    init commit

---
+++ FireEngine/FireEngine.cpp
@@ -0,0 +1,12 @@
+﻿// FireEngine.cpp : Defines the entry point for the application.
+//
+
+#include "FireEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
